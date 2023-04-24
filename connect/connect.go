1736e309642e06082232cc87a92c7d7d5d1231b2
Sergey Ryazanov <majo78@mail.ru>
2023-04-24 15:35:45 +0300

    Add API connector

--- connect/connect.go
+++ connect/connect.go
@@ -1,7 +1,32 @@
 package connect
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func Hello2() {
 	fmt.Println("Hello from connect")
+}
+
+func Init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func GetApi() {
+	fmt.Println("----------------------")
+	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
+	if exists {
+		fmt.Println("apiKey exist")
+	}
+
+	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
+	if exexists {
+		fmt.Println("secretKey exist")
+	}
 }
