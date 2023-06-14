26ae2af4d921de13c79670b299ac241694e7ae8a
Sergey Ryazanov <majo78@mail.ru>
2023-06-14 04:05:33 +0300

    Add lines for the header func

--- checkstartposition/checkstartposition.go
+++ checkstartposition/checkstartposition.go
@@ -3,5 +3,8 @@ package checkstartposition
 import "fmt"
 
 func Checkstartposition() {
+	fmt.Println("-------------------------")
 	fmt.Println("Hello checkstartposition!")
+	fmt.Println("-------------------------")
+
 }
