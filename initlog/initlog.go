0d1f6df46721e01d9d4b9c37007d0954d9563f60
Sergey Ryazanov <majo78@mail.ru>
2023-09-09 08:06:11 +0300

    Revert "feat(initlog): Add func NewFuturesClient"
    This reverts commit 703e4fa0cecfce21f844a7ff24c8109a586be902.
    feat(initlog): back to the last commit

--- initlog/initlog.go
+++ initlog/initlog.go
@@ -8,108 +8,10 @@ import (
 	"strconv"
 
 	"github.com/adshao/go-binance/v2"
-	"github.com/adshao/go-binance/v2/futures"
 	"github.com/joho/godotenv"
 )
 
-// func Init() {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("No.env file found.")
-// 	}
-// 	log.Println(".env file loaded.")
-
-// 	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
-// 	if !exists {
-// 		log.Fatal("BINANCE_API_KEY not set")
-// 	}
-
-// 	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
-// 	if !exexists {
-// 		log.Fatal("BINANCE_SECRET_KEY not set")
-// 	}
-
-// 	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
-
-// 	accountBalance, err := futuresClient.NewGetAccountService().Do(context.Background())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	accVar, _ := json.Marshal(accountBalance)
-
-// 	type Account struct {
-// 		FeeTier                     int    `json:"feeTier"`
-// 		CanTrade                    bool   `json:"canTrade"`
-// 		CanDeposit                  bool   `json:"canDeposit"`
-// 		CanWithdraw                 bool   `json:"canWithdraw"`
-// 		UpdateTime                  int64  `json:"updateTime"`
-// 		TotalInitialMargin          string `json:"totalInitialMargin"`
-// 		TotalMaintMargin            string `json:"totalMaintMargin"`
-// 		TotalWalletBalance          string `json:"totalWalletBalance"`
-// 		TotalUnrealizedProfit       string `json:"totalUnrealizedProfit"`
-// 		TotalMarginBalance          string `json:"totalMarginBalance"`
-// 		TotalPositionInitialMargin  string `json:"totalPositionInitialMargin"`
-// 		TotalOpenOrderInitialMargin string `json:"totalOpenOrderInitialMargin"`
-// 		TotalCrossWalletBalance     string `json:"totalCrossWalletBalance"`
-// 		TotalCrossUnPnl             string `json:"totalCrossUnPnl"`
-// 		AvailableBalance            string `json:"availableBalance"`
-// 		MaxWithdrawAmount           string `json:"maxWithdrawAmount"`
-// 	}
-
-// 	var acc Account
-// 	json.Unmarshal(accVar, &acc)
-// 	accountStart := 18.149229049682617 + 7.53667852 + 11.86 + 11.97 - 5
-// 	accountNowString := acc.AvailableBalance
-// 	if accountNowFloat, err := strconv.ParseFloat(accountNowString, 32); err == nil {
-// 		log.Println("-------------------Balance usdt----------------------")
-// 		log.Println(accountStart, "- start")
-// 		log.Println(accountNowFloat, "- now")
-// 		log.Println("-----------------------------------------------------")
-// 	}
-// }
-
-// func NewFuturesClient() *futures.Client {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("No.env file found.")
-// 	}
-// 	log.Println(".env file loaded.")
-
-// 	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
-// 	if !exists {
-// 		log.Fatal("BINANCE_API_KEY not set")
-// 	}
-
-// 	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
-// 	if !exexists {
-// 		log.Fatal("BINANCE_SECRET_KEY not set")
-// 	}
-
-// 	return binance.NewFuturesClient(apiKey, secretKey)
-// }
-
-type FuturesBinanceClient struct {
-	futuresClient *futures.Client
-}
-
-type Account struct {
-	FeeTier                     int    `json:"feeTier"`
-	CanTrade                    bool   `json:"canTrade"`
-	CanDeposit                  bool   `json:"canDeposit"`
-	CanWithdraw                 bool   `json:"canWithdraw"`
-	UpdateTime                  int64  `json:"updateTime"`
-	TotalInitialMargin          string `json:"totalInitialMargin"`
-	TotalMaintMargin            string `json:"totalMaintMargin"`
-	TotalWalletBalance          string `json:"totalWalletBalance"`
-	TotalUnrealizedProfit       string `json:"totalUnrealizedProfit"`
-	TotalMarginBalance          string `json:"totalMarginBalance"`
-	TotalPositionInitialMargin  string `json:"totalPositionInitialMargin"`
-	TotalOpenOrderInitialMargin string `json:"totalOpenOrderInitialMargin"`
-	TotalCrossWalletBalance     string `json:"totalCrossWalletBalance"`
-	TotalCrossUnPnl             string `json:"totalCrossUnPnl"`
-	AvailableBalance            string `json:"availableBalance"`
-	MaxWithdrawAmount           string `json:"maxWithdrawAmount"`
-}
-
-func NewFuturesClient() (*FuturesBinanceClient, error) {
+func Init() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No.env file found.")
 	}
@@ -117,24 +19,40 @@ func NewFuturesClient() (*FuturesBinanceClient, error) {
 
 	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
 	if !exists {
-		log.Panicln("BINANCE_API_KEY not set")
+		log.Fatal("BINANCE_API_KEY not set")
 	}
 
 	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
 	if !exexists {
-		log.Panicln("BINANCE_SECRET_KEY not set")
+		log.Fatal("BINANCE_SECRET_KEY not set")
 	}
 
 	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
-	return &FuturesBinanceClient{futuresClient: futuresClient}, nil
-}
 
-func (f *FuturesBinanceClient) Init() error {
-	accountBalance, err := f.futuresClient.NewGetAccountService().Do(context.Background())
+	accountBalance, err := futuresClient.NewGetAccountService().Do(context.Background())
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	accVar, _ := json.Marshal(accountBalance)
+
+	type Account struct {
+		FeeTier                     int    `json:"feeTier"`
+		CanTrade                    bool   `json:"canTrade"`
+		CanDeposit                  bool   `json:"canDeposit"`
+		CanWithdraw                 bool   `json:"canWithdraw"`
+		UpdateTime                  int64  `json:"updateTime"`
+		TotalInitialMargin          string `json:"totalInitialMargin"`
+		TotalMaintMargin            string `json:"totalMaintMargin"`
+		TotalWalletBalance          string `json:"totalWalletBalance"`
+		TotalUnrealizedProfit       string `json:"totalUnrealizedProfit"`
+		TotalMarginBalance          string `json:"totalMarginBalance"`
+		TotalPositionInitialMargin  string `json:"totalPositionInitialMargin"`
+		TotalOpenOrderInitialMargin string `json:"totalOpenOrderInitialMargin"`
+		TotalCrossWalletBalance     string `json:"totalCrossWalletBalance"`
+		TotalCrossUnPnl             string `json:"totalCrossUnPnl"`
+		AvailableBalance            string `json:"availableBalance"`
+		MaxWithdrawAmount           string `json:"maxWithdrawAmount"`
+	}
 
 	var acc Account
 	json.Unmarshal(accVar, &acc)
@@ -146,5 +64,4 @@ func (f *FuturesBinanceClient) Init() error {
 		log.Println(accountNowFloat, "- now")
 		log.Println("-----------------------------------------------------")
 	}
-	return nil
 }
