0a3970d8578425b50810e63f1c3dd377815c0eaa
MajotraderLucky <majohead@gmail.com>
2023-10-15 00:43:33 +0300

    feat(orderinfolog): Add GetOpenOrdersInfoJson func

--- orderinfolog/orderinfolog.go
+++ orderinfolog/orderinfolog.go
@@ -2,6 +2,7 @@ package orderinfolog
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
@@ -9,6 +10,7 @@ import (
 )
 
 func Hello() {
+	log.Println("---------------------------------------")
 	log.Println("orderinfolog package has been started")
 }
 
@@ -39,5 +41,47 @@ func GetOpenOrdersInfo() {
 	for _, o := range openOrders {
 		log.Println(o)
 		log.Println(len(openOrders), "orders have been opened")
+	}
+}
+
+type Output struct {
+	Orders    []Order `json:"orders"`
+	NumOpened int     `json:"numOpened"`
+}
+
+func GetOpenOrdersInfoJson() {
+	apiKey, secretKey := GetAPIKeys()
+
+	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
+
+	// Предположим, что функция NewListOpenOrdersService() возвращает []Order
+	openOrders, err := futuresClient.NewListOpenOrdersService().Symbol("BTCUSDT").
+		Do(context.Background())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	filePath := "logs/orders.json"
+
+	// Создайте файл для записи
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Создайте экземпляр Output и добавьте заказы и количество открытых заказов
+	output := &Output{
+		Orders:    openOrders,
+		NumOpened: len(openOrders),
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t") // для красивого вывода JSON
+
+	if err := encoder.Encode(output); err != nil {
+		log.Println(err)
 	}
 }
