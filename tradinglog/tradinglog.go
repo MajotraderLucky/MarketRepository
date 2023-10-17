e5f1340987e19f67c5723e6cc468f98f82b2537a
Sergey Ryazanov <majo78@mail.ru>
2023-10-17 17:17:51 +0300

    feat(tradinglog): Add GetFiboLevel function

--- tradinglog/tradinglog.go
+++ tradinglog/tradinglog.go
@@ -1,7 +1,20 @@
 package tradinglog
 
-import "log"
+import (
+	"log"
+
+	"github.com/MajotraderLucky/MarketRepository/klinesdata"
+)
 
 func Hello() {
 	log.Println("Hello, tradinglog!")
+}
+
+func GetFiboLevel() {
+	fibLevel, isHigher := klinesdata.IsAskPriceHigherThanLongFibRetLog()
+	if isHigher {
+		log.Printf("The ask price is higher than %s Fibonacci retracement level.\n", fibLevel)
+	} else {
+		log.Println("The ask price is not higher than any Fibonacci retracement level.")
+	}
 }
