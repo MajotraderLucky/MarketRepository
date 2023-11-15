b591335ae9eca2586eecf2e80edd782a946e1971
MajotraderLucky <majohead@gmail.com>
2023-11-16 01:07:05 +0300

    feat(transactions): Add CreateLimitOrder functions

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -1,7 +1,44 @@
 package transactions
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/adshao/go-binance/v2/futures"
+)
 
 func Hello() {
 	log.Println("Startng transaction...")
+}
+
+func GetAPIKeys() (string, string) {
+	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
+	if exists {
+		log.Println("apiKey checked")
+	}
+
+	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
+	if exexists {
+		log.Println("secretKey checked")
+	}
+	return apiKey, secretKey
+}
+
+func CreateLimitOrder(quantity string, price string) {
+	apiKey, secretKey := GetAPIKeys()
+	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
+
+	limitOrder, err := futuresClient.NewCreateOrderService().Symbol("BTCUSDT").
+		Side(futures.SideTypeBuy).Type(futures.OrderTypeLimit).
+		TimeInForce(futures.TimeInForceTypeGTC).Quantity(quantity).
+		Price(price).Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(limitOrder)
 }
